5151a4b22726351dad1799247c29012f73b45658
ya-3mameo <nao51.gren@gmail.com>
2020-11-03 19:30:31 +0900

    アソシエーション・バリデーション設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,18 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
+  with_options precence: true do
+    validates :title
+    validates :concept
+    validates :price
+    validates :prefecture
+    validates :visit_day
+    validates :price, format: { with: /\A[0-9]+\z/ }
+  end
+
+  validates :prefecture_id, numericality: { other_than: 1 }
 end
