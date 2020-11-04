688828775d2b70f71c5d709cc3405e8d1471db5d
ya-3mameo <nao51.gren@gmail.com>
2020-11-04 17:12:40 +0900

    投稿情報をDBに保存する

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,25 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.valid?
+      @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :image, :concept, :price, :prefecture_id, :visit_day).merge(user_id: current_user.id)
   end
 end
