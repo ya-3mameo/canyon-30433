77d64e26e4b51d853bc668f13e74f427e19e27d9
ya-3mameo <nao51.gren@gmail.com>
2020-11-10 16:40:57 +0900

    アクセス情報を保存

--- app/controllers/accesses_controller.rb
+++ app/controllers/accesses_controller.rb
@@ -3,11 +3,14 @@ class AccessesController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.find(params[:tweet_id])
     @access = Access.new
   end
 
   def create
-    @access = Access.new(acccess_params)
+    @tweet = Tweet.find(params[:tweet_id])
+    @access = Access.new(access_params)
+    binding.pry
     if @access.valid?
       @access.save
       redirect_to root_path
@@ -17,7 +20,7 @@ class AccessesController < ApplicationController
   end
 
   private
-  def acccess_params
-    params.require(:access).permit(:postal_number, :prefecture_id, :city, :house_number, :building, :phone_number, :access_plan).merge(tweet_id: tweet.id)
+  def access_params
+    params.require(:access).permit(:postal_number, :prefecture_id, :city, :house_number, :building, :phone_number, :access_plan).merge(tweet_id: @tweet.id)
   end
 end
